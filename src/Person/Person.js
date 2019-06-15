5168d7e0a4733ae55000cfbc951ca7b06a63c078
ivan <iizdepskyy112@gmail.com>
2019-06-15 14:53:56 +0300

    React Manipulating the State

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,9 @@ import React from 'react';
 const person = (props) => {
     return(
         <div>
-    <p>I'm {props.name} and I am {props.age} years old!</p>
-    <p>{props.children}</p>
-    </div>
+        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        </div>
     )
 };
 
